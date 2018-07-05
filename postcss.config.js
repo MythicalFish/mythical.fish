e9fcc98e6125ea4d05075ee01f71a35a8155078a
jake <jake@mythical.fish>
2018-07-05 09:13:03 +0200

    Fix build errors

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,11 @@ const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
-    require('postcss-mixins')(),
     require('postcss-import')(),
+    tailwindcss('./tailwind.config.js'),
     require('postcss-cssnext')(),
     require('postcss-extend')(),
+    require('postcss-mixins')(),
     require('css-mqpacker')(),
-    tailwindcss('./tailwind.config.js')
   ]
 }
