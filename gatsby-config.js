a05e618730e450e6baf0a7e594c03b0682c77873
Jake Broughton <jake@mythical.fish>
2023-04-18 19:21:26 +0200

    Fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,12 @@ module.exports = {
   plugins: [
     { resolve: "gatsby-plugin-mdx" },
     { resolve: "gatsby-transformer-remark" },
-    // { resolve: "gatsby-plugin-netlify-cms" },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/assets/images/favicon.png",
+      },
+    },
     { resolve: `gatsby-plugin-postcss` },
     {
       resolve: "gatsby-source-filesystem",
